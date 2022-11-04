6aae3a04391d32f32e7b07909270a81791452ec0
Lukas <69743171+quick007@users.noreply.github.com>
2022-11-03 21:24:24 -0700

    test

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,7 @@ import { useState } from "react";
 export default function Home() {
 	return (
 		<div className="">
-			<h1 className="font-bold">Hello, Next.js!
-      </h1>
+			<h1 className="font-bold">Hello, Next.js!</h1>
 		</div>
 	);
 }
