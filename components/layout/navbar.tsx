fe08fc5f7ed2b3c0b735cdc4471c02919b783949
Lukas <69743171+quick007@users.noreply.github.com>
2022-11-05 19:11:31 -0700

    fix wierd errors + update nav a bit

--- components/layout/navbar.tsx
+++ components/layout/navbar.tsx
@@ -1,4 +1,8 @@
+import { useUser } from "@supabase/auth-helpers-react";
+import type { ReactNode } from "react";
+
 export default function Navbar() {
+	const user = useUser();
 	const tabs: Tab[] = [
 		{
 			name: "Home",
@@ -11,18 +15,36 @@ export default function Navbar() {
 	];
 
 	return (
-		<nav className="flex justify-between bg-gray-200 px-8 py-4">
-			<div className="flex space-x-6">
+		<nav className="flex items-center justify-between bg-gray-200 px-8 py-2.5">
+			<div className="flex items-center space-x-6">
 				{tabs.map((v, i) => (
-					<div
-						key={i}
-						className={`rounded-md  ${
-							v.selected ? "bg-gray-50 shadow-md" : "bg-gray-300"
-						} px-3 py-0.5 text-lg font-semibold`}
-					>
-						{v.name}
-					</div>
+					<>
+						<div
+							key={i}
+							className={`my-0.5  rounded-md ${
+								v.selected ? "bg-gray-50 shadow-md" : "bg-gray-300"
+							} px-3 py-0.5 text-lg font-semibold `}
+						>
+							{v.name}
+						</div>
+
+						{i == 1 && (
+							<div className="graydient h-10 w-[0.07rem]" key={-1}></div>
+						)}
+					</>
 				))}
+			</div>
+			<div className="flex flex-row-reverse space-x-6">
+				{user ? (
+					<img
+						src={user.user_metadata.picture}
+						className="h-9 rounded-full shadow-md shadow-black/25"
+					/>
+				) : (
+					<div className="bg-blue-500 px-4 py-1 font-medium text-white">
+						Login
+					</div>
+				)}
 			</div>
 		</nav>
 	);
@@ -33,3 +55,5 @@ interface Tab {
 	name: string;
 	selected: boolean;
 }
+
+function button(props: { icon: ReactNode; classes: string }) {}
