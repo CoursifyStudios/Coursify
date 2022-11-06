3489b613a0abb88b1de47088bd412bf45dd7449e
Lukas <69743171+quick007@users.noreply.github.com>
2022-11-05 21:22:09 -0700

    Finish navbar design

--- components/layout/layout.tsx
+++ components/layout/layout.tsx
@@ -4,7 +4,7 @@ import Navbar from "./navbar";
 
 export default function Layout(props: { children: ReactNode }) {
 	return (
-		<div className="flex min-h-screen flex-col">
+		<div className="flex min-h-screen flex-col text-gray-900 ">
 			<Navbar />
 			<div className="flex flex-1 flex-col items-start">{props.children}</div>
 			<Footer />
--- components/layout/navbar.tsx
+++ components/layout/navbar.tsx
@@ -1,5 +1,11 @@
 import { useUser } from "@supabase/auth-helpers-react";
 import type { ReactNode } from "react";
+import {
+	CalendarIcon,
+	MagnifyingGlassIcon,
+	MegaphoneIcon,
+} from "@heroicons/react/24/outline";
+import Link from "next/link";
 
 export default function Navbar() {
 	const user = useUser();
@@ -34,12 +40,23 @@ export default function Navbar() {
 					</>
 				))}
 			</div>
-			<div className="flex flex-row-reverse space-x-6">
+			<div className="flex flex-row-reverse items-center space-x-4 space-x-reverse">
 				{user ? (
-					<img
-						src={user.user_metadata.picture}
-						className="h-9 rounded-full shadow-md shadow-black/25"
-					/>
+					<>
+						<img
+							src={user.user_metadata.picture}
+							className="!ml-2 h-10 rounded-full shadow-md shadow-black/25"
+						/>
+						<ButtonIcon
+							icon={<MegaphoneIcon className="h-6 w-6" />}
+							to="/announcements"
+						/>
+						<ButtonIcon
+							icon={<CalendarIcon className="h-6 w-6" />}
+							to="/calendar"
+						/>
+						<ButtonIcon icon={<MagnifyingGlassIcon className="p- h-6 w-6" />} />
+					</>
 				) : (
 					<div className="bg-blue-500 px-4 py-1 font-medium text-white">
 						Login
@@ -56,4 +73,21 @@ interface Tab {
 	selected: boolean;
 }
 
-function button(props: { icon: ReactNode; classes: string }) {}
+function ButtonIcon(props: { icon: ReactNode; to?: string; classes?: string }) {
+	const r = (
+		<div
+			className={
+				"grid h-9 w-9 place-items-center rounded-full bg-gray-300 text-gray-800 " +
+				props.classes
+			}
+		>
+			{props.icon}
+		</div>
+	);
+
+	if (props.to) {
+		return <Link href={props.to}>{r}</Link>;
+	} else {
+		return r;
+	}
+}
