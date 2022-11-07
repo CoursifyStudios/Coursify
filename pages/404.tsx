b639f806b6c807925d5514df4f81f5c2a5e84e4d
Brandon <brandon@infotransit.app>
2022-11-06 17:40:19 -0800

    Added 404 Page

---
+++ pages/404.tsx
@@ -0,0 +1,7 @@
+export default function Custom404() {
+	return (
+		<h1 className="absolute top-1/2 left-1/2 max-w-none -translate-x-1/2 -translate-y-1/2 text-4xl">
+			404 - Bill You Did Something Wrong
+		</h1>
+	);
+}
