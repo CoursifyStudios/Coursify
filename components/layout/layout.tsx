32746707c4e328672abb2f743d1be8ff2f1ffaac
Brandon <brandon@infotransit.app>
2022-11-06 20:51:23 -0800

    Fixed dumb way to center div

--- components/layout/layout.tsx
+++ components/layout/layout.tsx
@@ -6,7 +6,7 @@ export default function Layout(props: { children: ReactNode }) {
 	return (
 		<div className="flex min-h-screen flex-col text-gray-900 ">
 			<Navbar />
-			<div className="flex flex-1 flex-col items-start">{props.children}</div>
+			<div className="flex flex-1 flex-col">{props.children}</div>
 			<Footer />
 		</div>
 	);
--- pages/404.tsx
+++ pages/404.tsx
@@ -1,6 +1,6 @@
 export default function Custom404() {
 	return (
-		<h1 className="absolute top-1/2 left-1/2 max-w-none -translate-x-1/2 -translate-y-1/2 text-4xl">
+		<h1 className="flex min-h-screen items-center justify-center text-4xl">
 			404 - Bill You Did Something Wrong
 		</h1>
 	);
