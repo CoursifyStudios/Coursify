bd86c3722b4752d87c13438bc7c3c8c26730f296
Brandon <brandon@infotransit.app>
2022-11-06 20:56:00 -0800

    Lukas fixed my bad css again

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,6 +1,6 @@
 export default function Custom404() {
 	return (
-		<h1 className="flex min-h-screen items-center justify-center text-4xl">
+		<h1 className="flex flex-grow items-center justify-center text-4xl">
 			404 - Bill You Did Something Wrong
 		</h1>
 	);
