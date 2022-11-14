df22465d8b5ccc8f87eccb0d5b3b259ffa980e95
Brandon <brandon@infotransit.app>
2022-11-14 08:44:42 -0800

    New simple 404

--- pages/404.tsx
+++ pages/404.tsx
@@ -5,26 +5,18 @@ export default function Custom404() {
 
 	return (
 		<div className="flex flex-grow flex-col items-center justify-center">
-			<div className="flex items-center rounded-md bg-slate-300 p-10">
-				<Whatever color="bg-yellow-400" />
-				<h1 className=" ml-8 text-7xl font-extrabold ">404</h1>
+			<div className="flex-row items-center justify-center rounded-md">
+				<h1 className="pb-10 text-9xl font-extrabold">Whoops...</h1>
+				<h2 className="mb-5 text-center text-xl">
+					It doesnt seem like this page exists
+				</h2>
 			</div>
 
 			<div
 				onClick={() => router.push("/")}
 				className="mt-8 cursor-pointer rounded-md bg-blue-500 py-2 px-5 font-medium text-gray-100 hover:bg-blue-300"
 			>
-				Back to home
-			</div>
-		</div>
-	);
-}
-function Whatever(props: { color: string }) {
-	return (
-		<div className="relative">
-			<div className={`h-8 w-8 animate-ping rounded-full ${props.color}`} />
-			<div className="absolute inset-0 grid place-items-center">
-				<div className={`h-8 w-8 rounded-full ${props.color}`} />
+				Go back home
 			</div>
 		</div>
 	);
