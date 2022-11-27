d857bbf5531c9e8bb032d5da6a9de8cd1f08fda7
IllGive <93675675+IllGive@users.noreply.github.com>
2022-11-27 14:40:41 -0800

    Classes SHOULD support solors
    The little pills should be colored based on the class data from supabase, but I can't test it because middleware is making fun of me.

--- components/classes/class.tsx
+++ components/classes/class.tsx
@@ -18,7 +18,7 @@ export function Class(props: { class: Class }) {
 					<h3 className="break-words text-xl font-semibold line-clamp-2">
 						{props.class.data.name}
 					</h3>
-					<ColoredPill color="blue">11:30 - 12:15</ColoredPill>
+					<ColoredPill color={props.class.data.color}>11:30 - 12:15</ColoredPill>
 				</div>
 				<p>Teacher name</p>
 				<div className="mt-auto">
