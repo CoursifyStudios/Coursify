9fc387b2d2de85126c24cd5427d57234e67847c2
Brandon H <brandon@coursify.studio>
2023-03-04 19:18:17 -0800

    Autolink functionality added

--- components/editors/richeditor.tsx
+++ components/editors/richeditor.tsx
@@ -56,6 +56,37 @@ const editorConfig = {
 	namespace: "My editor",
 };
 
+const URL_MATCHER =
+	/((https?:\/\/(www\.)?)|(www\.))[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/;
+
+const EMAIL_MATCHER =
+	/(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))/;
+
+const MATCHERS = [
+	(text: string) => {
+		const match = URL_MATCHER.exec(text);
+		return (
+			match && {
+				index: match.index,
+				length: match[0].length,
+				text: match[0],
+				url: match[0],
+			}
+		);
+	},
+	(text: string) => {
+		const match = EMAIL_MATCHER.exec(text);
+		return (
+			match && {
+				index: match.index,
+				length: match[0].length,
+				text: match[0],
+				url: `mailto:${match[0]}`,
+			}
+		);
+	},
+];
+
 export default function Editor({ editable }: { editable: boolean }) {
 	return (
 		<GrammarlyEditorPlugin clientId="client_HhHcuxVxKgaZMFYuD57U3V">
@@ -76,7 +107,7 @@ export default function Editor({ editable }: { editable: boolean }) {
 							<CodeHighlightPlugin />
 							<ListPlugin />
 							<LinkPlugin />
-							<AutoLinkPlugin matchers={[]} />
+							<AutoLinkPlugin matchers={MATCHERS} />
 							<ListMaxIndentLevelPlugin maxDepth={7} />
 							<MarkdownShortcutPlugin transformers={TRANSFORMERS} />
 						</div>
