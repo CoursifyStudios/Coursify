3220c5006afc59112ee6e71fb133d804586d7680
IllGive <93675675+IllGive@users.noreply.github.com>
2023-03-09 16:35:05 -0800

    removed a console.log

--- pages/profile/[profileid].tsx
+++ pages/profile/[profileid].tsx
@@ -39,7 +39,6 @@ export default function Profile() {
                     supabaseClient
                 );
                 setProfileGroups(groupsData);
-                console.log(groupsData);
 			}
 		})();
 	}, [router, supabaseClient, profileid]);
