7cfa7e8ffeb78e28ae8dc0701cd8f232dd461940
Lukas <69743171+quick007@users.noreply.github.com>
2023-03-24 16:34:59 -0700

    Update supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -3,6 +3,6 @@ import type { Database } from "./db/database.types";
 import type { GetResult } from "@supabase/postgrest-js/dist/module/select-query-parser";
 
 export default createClient<Database>(
-	"https://cdn.coursify.one",
+	process.env.NEXT_PUBLIC_SUPABASE_URL!,
 	process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 );
