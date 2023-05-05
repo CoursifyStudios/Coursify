935c61c5477937bb2c0efda97891c1073b53823c
IllGive <93675675+IllGive@users.noreply.github.com>
2023-05-05 14:18:06 -0700

    example

--- components/settings/pages/profile.tsx
+++ components/settings/pages/profile.tsx
@@ -1,7 +1,24 @@
 import { NextPage } from "next";
+import { useEffect, useState } from "react";
+import { UserDataType, getUserData } from "../../../lib/db/settings";
+import { useSupabaseClient, useUser } from "@supabase/auth-helpers-react";
 
 const Profile: NextPage<{}> = () => {
-	return <>test</>;
+    const supabase = useSupabaseClient();
+    const user = useUser();
+	const [userData, setUserData] = useState<UserDataType>();
+	useEffect(() => {
+		(async () => {
+            const data = await getUserData(supabase, user?.id!);
+            setUserData(data);
+        })();
+	}, [user, userData]);
+	return (
+		<div>
+			<span>{userData?.data?.full_name}</span>
+		</div>
+	);
 };
 
 export default Profile;
+
