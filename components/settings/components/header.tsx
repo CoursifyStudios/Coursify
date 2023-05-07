b753aaeeb2125454bcb4d224500592f308ec6080
Lukas <69743171+quick007@users.noreply.github.com>
2023-05-06 18:00:07 -0700

    fix spacing

--- components/settings/components/header.tsx
+++ components/settings/components/header.tsx
@@ -36,7 +36,7 @@ export const Header: NextPage<{
 			}
 		>
 			<h3 className="title-sm">{children}</h3>
-			<LinkIcon className="ml-2 h-4 w-4 text-gray-600 opacity-0 transition group-hover:opacity-100 dark:text-gray-400" />
+			<LinkIcon className="ml-1 mr-1 h-4 w-4 text-gray-600 opacity-0 transition group-hover:opacity-100 dark:text-gray-400" />
 			<div className="h-0.5 grow rounded-full bg-gray-200"></div>
 		</div>
 	);
