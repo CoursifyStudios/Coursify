4ce4b926427f95b7e26082713e3472a4c147d498
Lukas <69743171+quick007@users.noreply.github.com>
2023-05-07 22:05:13 -0700

    ...

--- components/complete/announcements/commenting.tsx
+++ components/complete/announcements/commenting.tsx
@@ -123,7 +123,7 @@ export const Commenting = ({
 										name="content"
 										type="text"
 										className="mt-2 min-h-[2.5rem] w-full resize-y rounded-3xl border-none bg-gray-300 px-4 py-2 !ring-0 dark:placeholder:text-gray-400"
-										placeholder="Add a comment.."
+										placeholder="Add a comment..."
 										autoFocus
 									></Field>
 								</label>
