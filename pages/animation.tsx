223b4b766dbfce2f16d835ac250c49a5d6bb46d0
Blocks <51055767+Blocksnmore@users.noreply.github.com>
2023-05-12 22:07:50 -0500

    Css lukas will love

--- pages/animation.tsx
+++ pages/animation.tsx
@@ -1,13 +1,32 @@
 import styles from "../styles/animation.module.scss";
 
 export default function Animation() {
+	// This is just proof of concept, lukas will make it look good later
 	return (
 		<>
 			<div className={styles.container}>
 				<div>
-					<div className={styles.spinner} />
+					<div className={styles.spinner}>
+						<div />
+					</div>
+					<div className={styles.over} />
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						fill="none"
+						viewBox="0 0 24 24"
+						strokeWidth={1.5}
+						stroke="currentColor"
+						className={styles.check}
+					>
+						<path
+							strokeLinecap="round"
+							strokeLinejoin="round"
+							d="M4.5 12.75l6 6 9-13.5"
+						/>
+					</svg>
 				</div>
 			</div>
 		</>
 	);
 }
+
