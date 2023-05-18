527658371bb3369a23bef55eab807364031ea017
Brandon H <brandon@coursify.studio>
2023-05-17 18:03:54 -0700

    Capacitor

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,12 +1,12 @@
-import { CapacitorConfig } from '@capacitor/cli';
+import { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
-  appId: 'com.coursify',
-  appName: 'coursify',
-  webDir: 'out',
-  server: {
-    androidScheme: 'https'
-  }
+	appId: "com.coursify",
+	appName: "coursify",
+	webDir: "out",
+	server: {
+		androidScheme: "https",
+	},
 };
 
 export default config;
