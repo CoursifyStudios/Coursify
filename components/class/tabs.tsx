919cb0510802372eeaef3e85cf29aa399992892b
IllGive <93675675+IllGive@users.noreply.github.com>
2023-07-07 09:33:43 -0700

    huh

--- components/class/tabs.tsx
+++ components/class/tabs.tsx
@@ -1,11 +1,22 @@
-import { HomeModernIcon } from "@heroicons/react/24/outline";
+import { HomeModernIcon, MegaphoneIcon } from "@heroicons/react/24/outline";
+import { ReactNode } from "react";
+import Home from "./home";
+import { AnnouncementsComponent } from "../complete/announcements/announcementsComponent";
 
-export const classTabs: {
-    name: string;
-	icon: ReactNode;
-	content: ReactNode;
-}[] = [
-    {name: "Home",
-icon: <HomeModernIcon>,
-    content: }
-]
+
+// export const classTabs: {
+//     name: string;
+// 	icon: ReactNode;
+// 	content: ReactNode;
+// }[] = [
+//     {
+//         name: "Home",
+//         icon: <HomeModernIcon></HomeModernIcon>,
+//         content: <Home></Home>,
+//     },
+//     {
+//         name: "Announcements",
+//         icon: <MegaphoneIcon></MegaphoneIcon>,
+//         content: <AnnouncementsComponent></AnnouncementsComponent>,
+//     }
+// ];
