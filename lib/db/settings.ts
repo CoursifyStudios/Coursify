5db7b06a2e842b1a05b8e2989579fec22ffc1735
Lukas <69743171+quick007@users.noreply.github.com>
2023-07-17 12:06:39 -0700

    request only needed data

--- lib/db/settings.ts
+++ lib/db/settings.ts
@@ -5,7 +5,11 @@ export const getUserData = async (
 	supabase: SupabaseClient<Database>,
 	profileid: string
 ) => {
-	return await supabase.from("users").select(`*`).eq("id", profileid).single();
+	return await supabase
+		.from("users")
+		.select(`id, name, email, bio, year`)
+		.eq("id", profileid)
+		.single();
 };
 
 export type UserDataType = Awaited<ReturnType<typeof getUserData>>;
