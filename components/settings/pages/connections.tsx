43266ab0ae5a5e0b7594fa1b268a1ccd1f5a094f
IllGive <93675675+IllGive@users.noreply.github.com>
2023-07-23 01:11:33 -0700

    Had to format AGAIN

--- components/settings/pages/connections.tsx
+++ components/settings/pages/connections.tsx
@@ -3,7 +3,8 @@ import { NextPage } from "next";
 const Connections: NextPage = () => {
 	return (
 		<div>
-			In the future, users will be able to connect other services to our platform here. Slack, Zoom, Discord and others have all been suggested.
+			In the future, users will be able to connect other services to our
+			platform here. Slack, Zoom, Discord and others have all been suggested.
 		</div>
 	);
 };
