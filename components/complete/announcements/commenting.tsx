259b77c86878aa80b4806aea56bd9856bc60a2a5
IllGive <93675675+IllGive@users.noreply.github.com>
2023-08-09 14:42:49 -0700

    editing and commenting
    very basic, UI could use some work (I'm admitting my problems, !)

--- components/complete/announcements/commenting.tsx
+++ components/complete/announcements/commenting.tsx
@@ -11,6 +11,13 @@ import {
 import { howLongAgo } from "../../../lib/misc/dates";
 import { useTabs } from "../../../lib/tabs/handleTabs";
 import { Button } from "../../misc/button";
+import MenuSelect from "@/components/misc/menu";
+import {
+	EllipsisVerticalIcon,
+	PencilIcon,
+	TrashIcon,
+} from "@heroicons/react/24/outline";
+import { Delete } from "./delete";
 
 export const Comment = ({
 	id,
@@ -39,116 +46,169 @@ export const Comment = ({
 	// const [showReplying, setShowReplying] = useState(false); not prod ready
 	const [editing, setEditing] = useState(false);
 	const [errorText, setErrorText] = useState("");
-	return (
-		<div className="ml-4">
-			<div className="flex items-center pt-1 ">
-				<Link
-					href={"/profile/" + author}
-					className="inline-flex shrink-0 items-center rounded-full px-1 py-0.5 hover:bg-gray-300"
-					onClick={() =>
-						newTab(
-							"/profile/" + author,
-							users.full_name.split(" ")[0] + "'s Profile"
-						)
-					}
-				>
-					<Image
-						src={users.avatar_url}
-						alt="User image"
-						className="h-5 w-5 rounded-full object-cover"
-						referrerPolicy="no-referrer"
-						width={20}
-						height={20}
-					/>
-					<p className="ml-1.5 mr-1 font-semibold text-gray-700">
-						{users.full_name}
-					</p>
-				</Link>
-				<p className="pl-1.5 text-gray-600 dark:text-gray-400">{time}</p>
-				{/* TODO: add an edit. share, and delete button here*/}
-			</div>
-			{/* editing is unavailable atm because the dropdown is gone */}
-			{editing ? (
-				<Formik
-					initialValues={{
-						content: text,
+	const [deleted, setDeleted] = useState(false);
+	const [showDeleting, setShowDeleting] = useState(false);
+	if (!deleted) {
+		return (
+			<div className="ml-4">
+				<Delete
+					open={showDeleting}
+					setOpen={setShowDeleting}
+					setDeleted={setDeleted}
+					supabase={supabase}
+					announcement={{
+						id: id,
+						author: author,
+						title: content,
+						content: null,
+						time: time,
+						type: type,
+						clone_id: null,
+						users: users,
 					}}
-					onSubmit={async (formData) => {
-						setErrorText("");
-						const newEditedAnnouncement = await editAnnouncement(
-							supabase,
-							//kind of confusing but a comment's content uses the title field
-							{ id: id, author: user!.id, title: content, clone_id: null },
-							{ title: formData.content, content: null }
-						);
-						if (newEditedAnnouncement.error) {
-							//TODO: better error handling
-							setErrorText(
-								"Something went wrong, and your comment could not be edited"
-							);
-						} else {
-							setErrorText("");
-							setEditing(false);
-							setText(formData.content);
+					classID={communityid}
+				/>
+				<div className="flex items-center pt-1 ">
+					<Link
+						href={"/profile/" + author}
+						className="inline-flex shrink-0 items-center rounded-full px-1 py-0.5 hover:bg-gray-300"
+						onClick={() =>
+							newTab(
+								"/profile/" + author,
+								users.full_name.split(" ")[0] + "'s Profile"
+							)
 						}
-					}}
-				>
-					{({ values }) => (
-						<Form className="focus:outline-none">
-							<label htmlFor="content">
-								<Field
-									component="textarea"
-									name="content"
-									type="text"
-									className="min-h-[2.5rem] w-full resize-y border-none bg-gray-300 px-4 py-2.5 !ring-0 dark:placeholder:text-gray-400"
-									placeholder="Enter your revised comment here"
-									autoFocus
-								></Field>
-							</label>
-							<ErrorMessage name="content" />
-							<div className="m-1 flex justify-end gap-2">
-								<Button
-									className="brightness-hover transition hover:bg-red-300"
-									onClick={() => setEditing(false)}
-								>
-									Cancel
-								</Button>
-								<button
-									className={`rounded-md bg-blue-500 px-4 py-1 font-semibold text-white ${
-										values.content.length < 1
-											? "cursor-not-allowed brightness-75"
-											: "brightness-hover"
-									}`}
-									type="submit"
-								>
-									Post
-								</button>
+					>
+						<Image
+							src={users.avatar_url}
+							alt="User image"
+							className="h-5 w-5 rounded-full object-cover"
+							referrerPolicy="no-referrer"
+							width={20}
+							height={20}
+						/>
+						<p className="ml-1.5 mr-1 font-semibold text-gray-700">
+							{users.full_name}
+						</p>
+					</Link>
+					<p className="pl-1.5 text-gray-600 dark:text-gray-400">{time}</p>
+					{user && author == user.id && (
+						<MenuSelect
+							items={[
+								{
+									content: (
+										<>
+											{" "}
+											Edit <PencilIcon className="h-5 w-5" />{" "}
+										</>
+									),
+									onClick: () => {
+										setEditing(true);
+									},
+								},
+								{
+									content: (
+										<>
+											{" "}
+											Delete <TrashIcon className="h-5 w-5" />{" "}
+										</>
+									),
+									onClick: () => {
+										setShowDeleting(true);
+									},
+								},
+							]}
+						>
+							<div className=" p-2 hover:bg-gray-200">
+								<EllipsisVerticalIcon className="h-6 w-6" />
 							</div>
-						</Form>
+						</MenuSelect>
 					)}
-				</Formik>
-			) : (
-				<p>{text}</p>
-			)}
-			{/* Replying UI is below. Replying is coming later! so uh. Yeah */}
-			{/* 
-			<button
-				className="ml-2"
-				onClick={() => {
-					setShowReplying(true);
-				}}
-			>
-				<p>{showReplying ? "Replying to " + users.full_name : "Reply"}</p>
-			</button>
-			{showReplying && (
-				<Commenting
-					communityid={communityid}
-					parentID={id!}
-					showMe={setShowReplying}
-				></Commenting>
-			)} */}
-		</div>
-	);
+				</div>
+				{editing ? (
+					<Formik
+						initialValues={{
+							content: text,
+						}}
+						onSubmit={async (formData) => {
+							setErrorText("");
+							const newEditedAnnouncement = await editAnnouncement(
+								supabase,
+								//kind of confusing but a comment's content uses the title field
+								{ id: id, author: user!.id, title: content, clone_id: null },
+								{ title: formData.content, content: null }
+							);
+							if (newEditedAnnouncement.error) {
+								//TODO: better error handling
+								setErrorText(
+									"Something went wrong, and your comment could not be edited"
+								);
+							} else {
+								setErrorText("");
+								setEditing(false);
+								setText(formData.content);
+							}
+						}}
+					>
+						{({ values }) => (
+							<Form className="focus:outline-none">
+								<label htmlFor="content">
+									<Field
+										component="textarea"
+										name="content"
+										type="text"
+										className="min-h-[2.5rem] w-full resize-y border-none bg-gray-300 px-4 py-2.5 !ring-0 dark:placeholder:text-gray-400"
+										placeholder="Enter your revised comment here"
+										autoFocus
+									></Field>
+								</label>
+								<ErrorMessage name="content" />
+								<div className="m-1 flex justify-end gap-2">
+									<Button
+										className="brightness-hover transition hover:bg-red-300"
+										onClick={() => setEditing(false)}
+									>
+										Cancel
+									</Button>
+									<button
+										className={`rounded-md bg-blue-500 px-4 py-1 font-semibold text-white ${
+											values.content.length < 1
+												? "cursor-not-allowed brightness-75"
+												: "brightness-hover"
+										}`}
+										type="submit"
+									>
+										Post
+									</button>
+								</div>
+							</Form>
+						)}
+					</Formik>
+				) : (
+					<p>{text}</p>
+				)}
+				{/* Replying UI is below. Replying is coming later! so uh. Yeah */}
+				{/* 
+                <button
+                    className="ml-2"
+                    onClick={() => {
+                        setShowReplying(true);
+                    }}
+                >
+                    <p>{showReplying ? "Replying to " + users.full_name : "Reply"}</p>
+                </button>
+                {showReplying && (
+                    <Commenting
+                        communityid={communityid}
+                        parentID={id!}
+                        showMe={setShowReplying}
+                    ></Commenting>
+                )} */}
+			</div>
+		);
+	} else {
+		return null;
+	}
 };
 
 export const Commenting = ({
