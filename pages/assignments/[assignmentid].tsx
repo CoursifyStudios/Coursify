ef9e8a78fdb98d404d12e3d00368f0193efa9c36
Brandon H <brandon@coursify.studio>
2023-08-10 09:47:25 -0700

    Dropdown placeholder teacher submissions

--- pages/assignments/[assignmentid].tsx
+++ pages/assignments/[assignmentid].tsx
@@ -391,40 +391,49 @@ const Post: NextPageWithLayout = () => {
 						<div className="flex">
 							<div className="flex w-64 my-2 mt-4 flex-col">
 								<div className="w-full ">
-									<div className=" w-full rounded-xl p-2">
-										<Disclosure>
+									<div className=" w-full rounded-xl ">
+										<Disclosure as="div" className="">
 											{({ open }) => (
 												<>
-													<Disclosure.Button className="flex w-full justify-between rounded-lg bg-purple-100 px-4 py-2 text-left text-sm font-medium text-purple-900 hover:bg-purple-200 focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75">
+													<Disclosure.Button className="flex w-full justify-between rounded-lg bg-gray-200 px-4 py-2 text-left text-sm font-medium">
 														<span>Ungraded</span>
 														<ChevronUpIcon
 															className={`${
 																open ? "rotate-180 transform" : ""
-															} h-5 w-5 text-purple-500`}
+															} h-5 w-5 `}
 														/>
 													</Disclosure.Button>
-													<Disclosure.Panel className="px-4 pt-4 pb-2 text-sm text-gray-500">
-														If youre unhappy with your purchase for any reason,
-														email us within 90 days and well refund you in full,
-														no questions asked.
-													</Disclosure.Panel>
+													<Disclosure.Panel className="px-4 pt-4 pb-2 text-sm"></Disclosure.Panel>
 												</>
 											)}
 										</Disclosure>
 										<Disclosure as="div" className="mt-2">
 											{({ open }) => (
 												<>
-													<Disclosure.Button className="flex w-full justify-between rounded-lg bg-purple-100 px-4 py-2 text-left text-sm font-medium text-purple-900 hover:bg-purple-200 focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75">
-														<span>GradedÍ</span>
+													<Disclosure.Button className="flex w-full justify-between rounded-lg bg-gray-200 px-4 py-2 text-left text-sm font-medium">
+														<span>Not Submitted</span>
 														<ChevronUpIcon
 															className={`${
 																open ? "rotate-180 transform" : ""
-															} h-5 w-5 text-purple-500`}
+															} h-5 w-5 `}
 														/>
 													</Disclosure.Button>
-													<Disclosure.Panel className="px-4 pt-4 pb-2 text-sm text-gray-500">
-														No.
-													</Disclosure.Panel>
+													<Disclosure.Panel className="px-4 pt-4 pb-2 text-sm"></Disclosure.Panel>
+												</>
+											)}
+										</Disclosure>
+										<Disclosure as="div" className="mt-2">
+											{({ open }) => (
+												<>
+													<Disclosure.Button className="flex w-full justify-between rounded-lg bg-gray-200 px-4 py-2 text-left text-sm font-medium">
+														<span>Graded</span>
+														<ChevronUpIcon
+															className={`${
+																open ? "rotate-180 transform" : ""
+															} h-5 w-5 `}
+														/>
+													</Disclosure.Button>
+													<Disclosure.Panel className="px-4 pt-4 pb-2 text-sm"></Disclosure.Panel>
 												</>
 											)}
 										</Disclosure>
