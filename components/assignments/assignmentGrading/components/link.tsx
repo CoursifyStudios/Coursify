32cb34618aa129cc1746ead1b6fe636f3e8290fb
Lukas <69743171+quick007@users.noreply.github.com>
2023-08-13 20:29:38 -0700

    Update link.tsx
    Co-Authored-By: Bloxs <51055767+Blocksnmore@users.noreply.github.com>

--- components/assignments/assignmentGrading/components/link.tsx
+++ components/assignments/assignmentGrading/components/link.tsx
@@ -1,6 +1,7 @@
 import { TeacherAssignmentResponse } from "@/lib/db/assignments/assignments";
 import { SubmissionLink } from "../../assignmentPanel/submission.types";
 import { ColoredPill } from "@/components/misc/pill";
+import { useState } from "react";
 
 const LinkGrading = ({
 	assignmentData,
@@ -9,19 +10,44 @@ const LinkGrading = ({
 	assignmentData: TeacherAssignmentResponse;
 	submission: SubmissionLink;
 }) => {
+	const [selectedLink, setSelectedLink] = useState(0);
+
 	return (
 		<>
 			<div className="flex flex-wrap mb-2 gap-2">
 				{submission.links.map((link, i) => (
-					<ColoredPill key={i} color={`gray`} hoverState>
-						{link}
-					</ColoredPill>
+					<div onClick={() => setSelectedLink(i)} key={i}>
+						<ColoredPill
+							className={` brightness-hover ${
+								i == selectedLink ? "brightness-focus" : "!bg-gray-200"
+							} cursor-pointer`}
+							hoverState
+						>
+							{link}
+						</ColoredPill>
+					</div>
 				))}
 			</div>
 			<div className="px-4 py-2 bg-backdrop-200 rounded-t-xl flex">
 				<p className="font-medium mr-2">Link: </p>
-				<span className="truncate max-w-sm">{submission.links}</span>
+				<span className="truncate max-w-sm">
+					{submission.links[selectedLink]}
+				</span>
+				<a
+					href={submission.links[selectedLink]}
+					className="ml-auto"
+					target="_blank"
+				>
+					<ColoredPill color={`gray`} hoverState>
+						Open Link
+					</ColoredPill>
+				</a>
 			</div>
+			<iframe
+				src={submission.links[selectedLink]}
+				allow="fullscreen"
+				className="h-96 rounded-b-xl bg-backdrop-200"
+			></iframe>
 		</>
 	);
 };
