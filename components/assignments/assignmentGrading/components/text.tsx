221d14ad48767c1ce8d3c80c01c64e62e03c1d35
Lukas <69743171+quick007@users.noreply.github.com>
2023-08-21 22:10:48 -0700

    show chars/words in text

--- components/assignments/assignmentGrading/components/text.tsx
+++ components/assignments/assignmentGrading/components/text.tsx
@@ -4,8 +4,9 @@ import {
 	SubmissionText,
 } from "../../assignmentPanel/submission.types";
 import { ColoredPill } from "@/components/misc/pill";
-import { useState } from "react";
+import { useMemo, useState } from "react";
 import dynamic from "next/dynamic";
+import { AssignmentText } from "../../assignmentCreation/three/settings.types";
 
 const Editor = dynamic(() => import("@/components/editors/richeditor"));
 
@@ -16,6 +17,17 @@ const TextGrading = ({
 	assignmentData: TeacherAssignmentResponse;
 	submission: SubmissionText;
 }) => {
+	const [text, setText] = useState(
+		typeof submission.content == "string" ? submission.content : ""
+	);
+
+	const settings = assignmentData.data?.settings as unknown as AssignmentText;
+
+	const textLength = useMemo(() => {
+		if (settings.trueWhenChars) return text.length;
+		else return (text.match(/\S+/g) || []).length;
+	}, [text, settings]);
+
 	return (
 		<>
 			<div className="p-4 bg-backdrop-200 rounded-xl flex">
@@ -27,9 +39,17 @@ const TextGrading = ({
 						editable={false}
 						initialState={submission.content}
 						className=" "
+						updateRaw={setText}
 					/>
 				)}
 			</div>
+			<ColoredPill color="gray" className="mt-2 ml-auto text-sm">
+				<div>
+					{textLength}
+					{settings.maxChars && ` / ${settings.maxChars}`}{" "}
+					{settings.trueWhenChars ? "Characters" : "Words"}
+				</div>
+			</ColoredPill>
 		</>
 	);
 };
