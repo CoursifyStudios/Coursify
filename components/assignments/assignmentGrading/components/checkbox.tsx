cd84da121a3b452d37559b7847d14c90fe1b8f08
Lukas <69743171+quick007@users.noreply.github.com>
2023-08-23 15:06:14 -0700

    fix checkbox

--- components/assignments/assignmentGrading/components/checkbox.tsx
+++ components/assignments/assignmentGrading/components/checkbox.tsx
@@ -6,6 +6,7 @@ import {
 import { ColoredPill } from "@/components/misc/pill";
 import { useState } from "react";
 import { AssignmentCheckoff } from "../../assignmentCreation/three/settings.types";
+import { CheckIcon } from "@heroicons/react/24/outline";
 
 const CheckboxGrading = ({
 	assignmentData,
@@ -20,16 +21,27 @@ const CheckboxGrading = ({
 
 	return (
 		<>
-			{submission.checkboxes}
 			{(
 				assignmentData.data.settings as unknown as AssignmentCheckoff
 			).checkboxes.map((checkbox) => {
 				const checked =
-					submission &&
 					submission.checkboxes &&
 					submission.checkboxes.includes(checkbox.step);
-
-				<div key={checkbox.step}>{checkbox.teacher}</div>;
+				return (
+					<div key={checkbox.step} className="flex p-3 border rounded-xl mb-2">
+						<div
+							className={`checkbox mr-3 mt-1 h-5 min-w-[1.25rem] rounded border-2 border-gray-300 transition  ${
+								checked ? "bg-gray-300 " : "dark:bg-neutral-950"
+							}`}
+						>
+							{checked && <CheckIcon className="h-4 w-4" />}
+						</div>
+						<div className="grow">
+							<h3 className="font-semibold">{checkbox.name}</h3>
+							<p className="text-sm">{checkbox.description}</p>
+						</div>
+					</div>
+				);
 			})}
 		</>
 	);
